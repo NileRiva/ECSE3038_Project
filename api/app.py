266198c90eb8e64b78a53e9b98986d0e804ba2d9
NileRiva <niletributary@gmail.com>
2023-05-19 01:32:59 -0500

    All Methods Working from Postman without Internal Server Error

--- api/app.py
+++ api/app.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 load_dotenv() #Nile Code, loads things from the coding environment
 client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('MONGO_CONNECTION_STRING'))#Attempt at hiding URL - Nile
-db = client.temperaturedb
+db = client.statedb
 db2 = client.settingsdb
 
 pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
@@ -36,63 +36,71 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#PUT 
-@app.put("/api/temperature",status_code=204)
-async def set_temp(request:Request):
+#POST Because we actually want a record of the previous ones  
+@app.post("/api/state",status_code=201) #cool that I changed it to post ?
+async def set_state(request:Request):
     
-    temperature = await request.json()
-    elements = await db["temperatures"].find().to_list(1)
+    state = await request.json()
+    state["datetime"]=datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
-    if len(elements)==0:
-         new_temp = await db["temperatures"].insert_one(temperature)
-         patched_temp = await db["temperatures"].find_one({"_id": new_temp.inserted_id }) #updated_tank.upserted_id
-         return patched_temp
-    else:
-        id=elements[0]["_id"]
-        updated_temp= await db["temperatures"].update_one({"_id":id},{"$set": temperature})
-        patched_temp = await db["temperatures"].find_one({"_id": id}) #updated_tank.upserted_id
-        if updated_temp.modified_count>=1: 
-            return patched_temp
+    new_state = await db["states"].insert_one(state)
+    updated_state = await db["states"].find_one({"_id": new_state.inserted_id }) #updated_tank.upserted_id
+    if new_state.acknowledged == True:
+        return updated_state
     raise HTTPException(status_code=400,detail="Issue")
 
 #GET /data
 @app.get("/api/state")
 async def getstate():
-    currenttemp = await db["temperatures"].find().to_list(1)
-    fanstate = (float(currenttemp[0]["temperature"])>referencetemp) #Watch Formatting here
-    lightstate = (datetime.now(tz=pytz.timezone('America/New_York')).time()>sunset().time())
+
+    currentstate = await db["states"].find().to_list(1)
+    currentsettings = await db["settings"].find().to_list(1)
+
+    fanstate = ((float(currentstate[0]["temperature"])>float(currentsettings[0]["user_temp"])) and (currentstate[0]["presence"]))  #Watch Formatting here
+
+    timenow=datetime.strptime(datetime.strftime(datetime.now(),'%H:%M:%S'),'%H:%M:%S')
+    userlight=datetime.strptime(currentsettings[0]["user_light"],'%H:%M:%S')
+    lightoff=datetime.strptime(currentsettings[0]["light_time_off"],'%H:%M:%S')
+
+    lightstate = (timenow>userlight) and ((currentstate[0]["presence"])) and (timenow<lightoff)
+    #Print Statements for Debugging
+    print(datetime.strftime(datetime.now(),'%H:%M:%S'))
+    print(currentsettings[0]["user_light"])
+    print(currentsettings[0]["light_time_off"])
+    
     Dictionary ={"fan":fanstate, "light":lightstate}
     return Dictionary
 
 def sunset():
     sunsetresponse=requests.get(f'https://api.sunrise-sunset.org/json?lat=18.1096&lng=-77.2975&date=today')
     sunsetjson = sunsetresponse.json()
-    sunsettimedate = sunsetjson["results"]["sunset"]
-    sunsettimedate = datetime.strptime(sunsettimedate,'%I:%M:%S %p')
+    sunsettimedate = sunsetjson["results"]["sunset"] #Returns Sunset in UTC Time
+    sunsettimedate = datetime.strptime(sunsettimedate,'%I:%M:%S %p') + timedelta(hours=-5) #Converting form UTC to GMT-5 (Our Timezone)
+    sunsettimedate = datetime.strftime(sunsettimedate,'%H:%M:%S') 
     return sunsettimedate
 
 #GET /Graph
 @app.get("/graph", status_code=200)
-async def graphpoints():
-    n = request.args['size']
-    temparray = await db["temperatures"].find().to_list(n)
-    return temparray
+async def graphpoints(request:Request,size: int):
+    n = size
+    statearray = await db["states"].find().to_list(n)
+    return statearray
 
 
 #PUT /Settings
-@app.put("/settings",status_code=204)
+@app.put("/settings",status_code=200)
 async def setting(request:Request):
     
     setting = await request.json()
     elements = await db["settings"].find().to_list(1)
-
+    mod_setting = {}
     mod_setting["user_temp"]=setting["user_temp"]
-    if setting["user_light"]=="sunset":
+    if setting["user_light"]== "sunset":
         mod_setting["user_light"]=sunset()
     else:
-        mod_setting["user_light"]=setting["user_light"]
+        mod_setting["user_light"] = setting["user_light"]
     
-    mod_setting["light_time_off"]= ToString(datetime.strptime(setting["user_light"],'%H:%M:%S')+parse_time(setting["light_duration"])) #Convert to string ?
+    mod_setting["light_time_off"]= (datetime.strptime(mod_setting["user_light"],'%H:%M:%S')+parse_time(setting["light_duration"])).strftime("%H:%M:%S") #Convert to string ?
     
 
     if len(elements)==0:
@@ -120,3 +128,4 @@ def parse_time(time_str):
         if param:
             time_params[name] = int(param)
     return timedelta(**time_params)
+
