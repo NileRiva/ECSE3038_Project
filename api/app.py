f717bea2f6dd0b7499e58b94e70f661c517821ab
NileRiva <niletributary@gmail.com>
2023-05-21 02:17:40 -0500

    Made Some changes to the time formats. Please mark previous version if this is breaking.

--- api/app.py
+++ api/app.py
@@ -42,7 +42,6 @@ async def set_state(request:Request):
     
     state = await request.json()
     state["datetime"]=(datetime.now()+timedelta(hours=-5)).strftime('%Y-%m-%dT%H:%M:%S')
-
     new_state = await db["states"].insert_one(state)
     updated_state = await db["states"].find_one({"_id": new_state.inserted_id }) #updated_tank.upserted_id
     if new_state.acknowledged == True:
@@ -55,17 +54,17 @@ async def getstate():
     currentstate = await db["states"].find().sort("datetime",-1).to_list(1)
     #currentstate = await db["states"].find().skip((db["states"].collection.count()) - (db["states"].collection.count()-1)).to_list(1)
     currentsettings = await db["settings"].find().to_list(1)
-
     presence = currentstate[0]["presence"]
-    timenow=datetime.strptime(datetime.strftime(datetime.now()+timedelta(hours=-5),'%H:%M:%S'),'%H:%M:%S')
-    userlight=datetime.strptime(currentsettings[0]["user_light"],'%H:%M:%S')
-    lightoff=datetime.strptime(currentsettings[0]["light_time_off"],'%H:%M:%S')
+    
+    timenow=datetime.strptime(datetime.strftime(datetime.now()+timedelta(hours=-5),'%Y-%m-%dT%H:%M:%S'),'%Y-%m-%dT%H:%M:%S')
+    userlight=datetime.strptime(currentsettings[0]["user_light"],'%Y-%m-%dT%H:%M:%S')
+    lightoff=datetime.strptime(currentsettings[0]["light_time_off"],'%Y-%m-%dT%H:%M:%S')
 
     fanstate = ((float(currentstate[0]["temperature"])>float(currentsettings[0]["user_temp"])) and presence)  #Watch Formatting here
     lightstate = (timenow>userlight) and (presence) and (timenow<lightoff)
     
     #Print Statements for Debugging
-    print(datetime.strftime(datetime.now()+timedelta(hours=-5),'%H:%M:%S'))
+    print(datetime.strftime(datetime.now()+timedelta(hours=-5),'%Y-%m-%dT%H:%M:%S'))
     print(currentsettings[0]["user_light"])
     print(currentsettings[0]["light_time_off"])
     print(presence)
@@ -101,11 +100,14 @@ async def setting(request:Request):
     mod_setting = {}
     mod_setting["user_temp"]=setting["user_temp"]
     if setting["user_light"]== "sunset":
-        mod_setting["user_light"]=sunset()
+        timestring = sunset()
     else:
-        mod_setting["user_light"] = setting["user_light"]
-    
-    mod_setting["light_time_off"]= (datetime.strptime(mod_setting["user_light"],'%H:%M:%S')+parse_time(setting["light_duration"])).strftime("%H:%M:%S") #Convert to string ?
+        timestring = setting["user_light"]
+
+    mod_setting["user_light"]=(datetime.now().date()).strftime("%Y-%m-%dT")+timestring
+    mod_setting["light_time_off"]= (datetime.strptime(mod_setting["user_light"],'%Y-%m-%dT%H:%M:%S')+parse_time(setting["light_duration"]).strftime('%Y-%m-%dT%H:%M:%S'))
+    print(mod_setting["user_light"])
+    print(mod_setting["light_time_off"])
     
 
     if len(elements)==0:
